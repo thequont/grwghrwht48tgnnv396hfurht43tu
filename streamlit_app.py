cedd3a4f7f36ddee77bcfa9585a37352ee16d833
thequont <77597365+thequont@users.noreply.github.com>
2022-03-10 21:44:11 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,12 @@ import pandas as pd
 import plotly.express as px
 df = pd.DataFrame(pd.read_csv("data.csv"))
 #df['time'] = pd.to_datetime(df['time'])
-clist = df['ID'].unique()
+clist = df['id'].unique()
 st.write(clist)
 country = st.sidebar.selectbox("Select ID:",clist)
 st.header("T over time")
-fig = px.line(df[df['ID'] == ID], 
-    x = "time", y = "T", title = ID)
+fig = px.line(df[df['id'] == id], 
+    x = "time", y = "T", title = id)
 st.plotly_chart(fig)
 
 st.header("data dump")
