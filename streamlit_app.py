6e5a46b11d85ccebe379a425ab821f35b8008c54
thequont <77597365+thequont@users.noreply.github.com>
2022-03-10 21:29:24 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
+import plotly.figure_factory as ff
 df = pd.DataFrame(pd.read_csv("data.csv"))
 df['time'] = pd.to_datetime(df['time'])
 clist = df['ID'].unique()
 country = st.sidebar.selectbox("Select ID:",clist)
 st.header("T over time")
-fig = st.line(df[df['ID'] == ID], 
+fig = ff.line(df[df['ID'] == ID], 
     x = "time", y = "T", title = ID)
 st.plotly_chart(fig)
 
