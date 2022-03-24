58e3d313a5e1d444674fefc7f34047cdec06547f
thequont <77597365+thequont@users.noreply.github.com>
2022-03-24 16:14:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -8,17 +7,16 @@ df = pd.DataFrame(pd.read_csv("data.csv"))
 #df['time'] = pd.to_datetime(df['time'])
 clist = df['id_'].unique()
 id_ = st.sidebar.selectbox("Select ID:",clist)
-st.header("T over time")
-fig = px.scatter_3d(df[df['id_'] == id_], 
-    x = "time", y = "T", z="RSSI", title = str(id_))
-st.plotly_chart(fig)
 
-st.header("RSSI over time")
-figR = px.line(df[df['id_'] == id_], 
-    x = "time", y = "RSSI", title = str(id_))
-st.plotly_chart(figR)
+st.header("T over time")
+fig = px.line(df[df['id_'] == id_], 
+    x = "time", y = "T", title = str(id_))
+st.plotly_chart(fig)
 
 st.header("Raw Data")
 st.write(df)
+
+
+
 
 
